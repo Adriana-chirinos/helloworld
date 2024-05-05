fc89147f19c7af1ea236773aaee82e196d5dc50a
Adriana-chirinos <168022596+Adriana-chirinos@users.noreply.github.com>
2024-05-05 20:39:54 +0200

    Update api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -25,26 +25,5 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "3", "ERROR ADD"
         )
         
-    def test_api_multiply(self):
-        url = f"{BASE_URL}/calc/multiply/5/2"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-        self.assertEqual(
-            response.read().decode(), "10", "ERROR ADD"
-        )
-
-    def test_api_sqrt(self):
-        url = f"{BASE_URL_MOCK}/calc/sqrt/64"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-        self.assertEqual(
-            response.read().decode(), "8", "ERROR SQRT"
-            
-        )
-
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
